0c81cdbea9b811eca4de408e4a48525b97c8bffa
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-19 19:34:56 +0700

    Create AfishaManagerTest.java

---
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -0,0 +1,35 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Films;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AfishaManagerTest {
+    AfishaManager manager = new AfishaManager();
+    Films first = new Films(1, 1, "Name 1", "12.25.21", "Type film 1", 150);
+    Films second = new Films(2 , 2, "Name 2", "10.22.21", "Type film 2", 120);
+    Films third = new Films(3, 3, "Name 3", "09.09.21", "Type film 2", 90);
+    Films fo = new Films(4, 4, "Name 4", "08.08.21", "Type film 3", 180);
+
+    @Test
+    void add() {
+        int id = 4;
+        manager.add(fo);
+    }
+
+    @Test
+    void removeById() {
+       int idToRemove = 1;
+       manager.removeById(idToRemove);
+
+       Films[] actual = manager.getAll();
+       Films[] expected = new Films[]{third, second};
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void getAll() {
+    }
+}
