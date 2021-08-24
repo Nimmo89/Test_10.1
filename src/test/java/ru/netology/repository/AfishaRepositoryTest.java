99017769e44efa153423886b45cb6fa733a85fd5
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-24 23:31:26 +0700

    Create AfishaRepositoryTest.java

---
+++ src/test/java/ru/netology/repository/AfishaRepositoryTest.java
@@ -0,0 +1,24 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Films;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AfishaRepositoryTest {
+
+//    @Test
+//    void save() {
+//        Films[] expected = new Films[]{f1, f2, f5, f4};
+//        Films[] actual = manager.getAll();
+//        assertArrayEquals(expected,actual);
+//    }
+//
+//    @Test
+//    void findAll() {
+//    }
+//
+//    @Test
+//    void removeById() {
+//    }
+}
