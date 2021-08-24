cf726990e75cc93370469def63d33660337f96e3
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-24 23:07:30 +0700

    Create AfishaRepository.java

---
+++ src/main/java/ru/netology/repository/AfishaRepository.java
@@ -0,0 +1,33 @@
+package ru.netology.repository;
+
+import ru.netology.domain.Films;
+
+public class AfishaRepository {
+    private Films[] films = new Films[0];
+
+    public void save(Films film) {
+        Films[] tmp = new Films[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = film;
+        films = tmp;
+    }
+
+    public Films[] findAll() {
+        return films;
+    }
+
+    public void removeById(int id) {
+        int length = films.length - 1;
+        Films[] tmp = new Films[length];
+        int index = 0;
+        for (Films film : films) {
+            if (film.getId() != id) {
+                tmp[index] = film;
+                index++;
+            }
+        }
+        films = tmp;
+    }
+}
