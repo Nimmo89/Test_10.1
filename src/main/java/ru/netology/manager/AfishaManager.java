0660bfc973ee65fa57f5fcee0433ece28bcce600
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-24 19:13:59 +0700

    Update AfishaManager.java

--- src/main/java/ru/netology/manager/AfishaManager.java
+++ src/main/java/ru/netology/manager/AfishaManager.java
@@ -14,39 +14,35 @@ public class AfishaManager {
     }
 
     public void add(Films film) {
-        if (maxLenght < 10) {
-        // создаём новый массив размером на единицу больше
-            int length = films.length + 1;
-            Films[] tmp = new Films[length];
-            System.arraycopy(film, 0, tmp, 0, films.length);
-            // кладём последним наш элемент
-            int lastIndex = tmp.length - 1;
-            tmp[lastIndex] = film;
-            film = tmp;
+        Films[] tmp = new Films[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
         }
-    }
-
-    public void removeById(int id) {
-        // наивная реализация
-        int length = films.length - 1;
-        Films[] tmp = new Films[length];
-        int index = 0;
-        for (Films item : films) {
-            if (item.getId() != id) {
-                tmp[index] = item;
-                index++;
-            }
-        }
+        tmp[tmp.length - 1] = film;
         films = tmp;
     }
-    public Films[] getAll() {
-        Films[] result = new Films[films.length];
-        //перебираем массив в прямом порядке
-        //но кладём в результаты в обратном
-        for (int i = 0; i < result.length; i++) {
-            int index = films.length - i - 1;
-            result[i] = films[index];
-        }
-        return result;
-    }
+
+//    public void removeById(int id) {
+//        // наивная реализация
+//        int length = films.length - 1;
+//        Films[] tmp = new Films[length];
+//        int index = 0;
+//        for (Films item : films) {
+//            if (item.getId() != id) {
+//                tmp[index] = item;
+//                index++;
+//            }
+//        }
+//        films = tmp;
+//    }
+//    public Films[] getAll() {
+//        Films[] result = new Films[films.length];
+//        //перебираем массив в прямом порядке
+//        //но кладём в результаты в обратном
+//        for (int i = 0; i < result.length; i++) {
+//            int index = films.length - i - 1;
+//            result[i] = films[index];
+//        }
+//        return result;
+//    }
 }
