be42ae9f3008f23f2402f6a559356c1f0f5ad899
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-08-31 20:58:09 +0700

    Update AfishaManagerTest.java

--- src/test/java/ru/netology/manager/AfishaManagerTest.java
+++ src/test/java/ru/netology/manager/AfishaManagerTest.java
@@ -6,6 +6,7 @@ import ru.netology.domain.Films;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AfishaManagerTest {
+    private AfishaManager manager1 = new AfishaManager(5);
     private AfishaManager manager = new AfishaManager();
     Films f1 = new Films(1, 1, "Name1", "12.25.21", "TypeFilm1", 150);
     Films f2 = new Films(2, 2, "Name2", "10.22.21", "TypeFilm2", 120);
@@ -60,7 +61,7 @@ class AfishaManagerTest {
         manager.add(f11);
 
         Films[] expected = new Films[]{f0, f9, f8, f7, f6, f5, f4, f3, f2, f1};
-        Films[] actual = manager.findOver();
+        Films[] actual = manager.findOverMaxLenght();
         assertArrayEquals(expected,actual);
     }
 
@@ -77,7 +78,21 @@ class AfishaManagerTest {
         manager.add(f9);
 
         Films[] expected = new Films[]{f9, f8, f7, f6, f5, f4, f3, f2, f1};
-        Films[] actual = manager.findOver();
+        Films[] actual = manager.findOverMaxLenght();
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    void getAllOver5() {
+        manager1.add(f1);
+        manager1.add(f3);
+        manager1.add(f5);
+        manager1.add(f7);
+        manager1.add(f9);
+        manager1.add(f11);
+
+        Films[] expected = new Films[]{f9, f7, f5, f3, f1};
+        Films[] actual = manager1.findOverMaxLenght();
         assertArrayEquals(expected,actual);
     }
 }
